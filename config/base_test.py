852b9c758b9663ced907b01d223b6c3dde04fd86
podchasova11 <Pod4asova20@gmail.com>
2023-12-12 11:30:36 +0300

    added config/base_test

---
+++ config/base_test.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from libs.helper import Helper
+from data.credentials import Credentials
+from pages.login_page import LoginPage
+# from pages.dashboard_page import DashboardPage
+
+class BaseTest:
+
+    def setup(self):
+        self.login_page = LoginPage(self.driver)
+
+
+    # def setup(self):
+    #     self.login_page = LoginPage(self.driver)
+    #
+    #     # Libs
+    #     self.helper = Helper
+
+
+
+
+
