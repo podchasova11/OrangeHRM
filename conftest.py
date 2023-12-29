41ec4f5ec9e523b4bd5d044d2fb1d696ae452515
Mila Podchasova <Pod4asova20@gmail.com>
2023-12-29 23:48:48 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,23 +15,23 @@ def driver(request):
     yield
     driver.quit()
 
-# @pytest.fixture(autouse=True)
-# def get_driver(request):
-#     if os.environ["BROWSER"] == "chrome":
-#         options = Options()
-#         options.add_argument("--incognito")
-#         options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
-#         options.add_argument("--window-size=1920,1080")
-#         options.add_argument("-disable-blink-features=AutomationControlled")
-#         driver = webdriver.Chrome(options=options)
-#         request.cls.driver = driver
-#         yield
-#         driver.quit()
-#     elif os.environ["BROWSER"] == "safari":
-#         driver = webdriver.Safari()
-#         request.cls.driver = driver
-#         yield
-#         driver.quit()
+@pytest.fixture(autouse=True)
+def get_driver(request):
+    if os.environ["BROWSER"] == "chrome":
+        options = Options()
+        options.add_argument("--incognito")
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("-disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(options=options)
+        request.cls.driver = driver
+        yield
+        driver.quit()
+    elif os.environ["BROWSER"] == "safari":
+        driver = webdriver.Safari()
+        request.cls.driver = driver
+        yield
+        driver.quit()
 
 
 # def create_browser(driver="chrome"):
